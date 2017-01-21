41ab84afa672a849b97079b517c56194400e68d5
Valerie Boem <vboem@gmail.com>
2017-01-21 17:39:00 -0500

    Completed methods for Contact class.

--- contact.rb
+++ contact.rb
@@ -26,44 +26,66 @@ class Contact
 
   # This method should return all of the existing contacts
   def self.all
-
+    @@contacts
   end
 
   # This method should accept an id as an argument
   # and return the contact who has that id
-  def self.find
-
+  def self.find(id)
+    @@contacts.each do |contact|
+    if contact.id == id
+      return contact
+      end
+    end
   end
 
   # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
-  def update
-
+  def update(attribute, value)
+    case attribute
+      when "first_name"
+        self.first_name = value
+      when "last_name"
+        self.last_name = value
+      when "note"
+        self.note = value
+      when "email"
+        self.email = value
+    end
   end
 
   # This method should work similarly to the find method above
   # but it should allow you to search for a contact using attributes other than id
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
-  def self.find_by
-
+  def self.find_by(attribute, value)
+    found = []
+    @@contacts.each do |contact|
+      if contact.send(attribute) == value
+        found << contact
+      else
+      end
+    end
+    return found
   end
 
   # This method should delete all of the contacts
   def self.delete_all
-
+    remove = @@contacts.compact
+    puts remove.concat(@@contacts)
   end
 
   def full_name
+    "#{first_name} #{last_name}"
 
   end
 
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
-
+    @@contacts.delete(self)
   end
 
   # Feel free to add other methods here, if you need them.
