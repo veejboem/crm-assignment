a057b02187b098f571026c03e5586a76bfb0f2fd
Valerie Boem <vboem@gmail.com>
2017-01-23 14:26:39 -0500

    Added crm-tet-suite. Also further work on CRM file.

--- crm.rb
+++ crm.rb
@@ -33,9 +33,10 @@ attr_reader :name
     when 2 then modify_existing_contact
     when 3 then delete_contact
     when 4 then display_all_contacts
-    when 5 then search_by_attribute call
+    when 5 then search_by_attribute
     when 6 then exit
   end
+end
 
   def add_new_contact
     puts "Enter First Name: "
@@ -52,27 +53,23 @@ attr_reader :name
 
     Contact.create(first_name, last_name, email, note)
   end
-end
-
 
   def modify_existing_contact
-    contact = search_by_attribute
-    if contact.count == 0
-      "Oops, that didn't work! Try entering a different number."
-    else
-      puts "[1] Modify first name"
-      puts "[2] Modify last name"
-      puts "[3] Modify email"
-      puts "[4] Modify note"
-      puts "[5] Exit"
-      puts "Enter a number:  "
-      value = gets.chomp.to_i
-      case value
-        when 1
-          "What would you like to change the first name to?"
-          first_name = gets.chomp
-          contact[0].first_name = first_name_new
-          "You have changed this contact's name to #{first_name}."
+    puts "We need to find the contact you want to modify."
+      contact = search_by_attribute
+        puts "[1] Modify first name"
+        puts "[2] Modify last name"
+        puts "[3] Modify email"
+        puts "[4] Modify note"
+        puts "[5] Exit"
+        puts "Enter a number:  "
+        value = gets.chomp.to_i
+        case value
+          when 1
+            "What would you like to change the first name to?"
+            first_name = gets.chomp
+            contact[0].first_name = first_name_new
+            "You have changed this contact's name to #{first_name}."
         when 2
           "What would you like to change the last name to?"
           last_name = gets.chomp
@@ -93,30 +90,59 @@ end
       return "Oops, that didn't work! Try entering a different number."
     end
   end
-end
+
 
   def delete_contact
+    puts "We need to find the contact you want to delete."
     contact_to_remove = search_by_attribute
-    puts "Which contact would you like to remove?"
-    # contact_to_remove = gets.chomp
     # contact_to_remove.delete
-    self.delete
+    Contact.delete
+    # self.delete
   end
-end
 
   def display_all_contacts
-    #return the entire array (call on a class variable)
-    all_contacts = Contact.all
-    all_contacts.each do |display|
-    puts "ID: #{display.id} Name: #{display.first_name} Last Name: #{display.last_name} Email: #{display.email} Note: #{display.note}"
-  end
-end
+    #return the entire array (call on a class Contact)
+    input = Contact.all
+    input.each do |list|
+    puts "ID: #{list.id}"
+    puts "First name: #{list.first_name}"
+    puts "Last name: #{list.last_name}"
+    puts "Email: #{list.email}"
+    puts "Note: #{list.note}"
 
-  def search_by_attribute
-    .each do | |
-      CRM.include?
-
-    #iterate using each which includes the arguement attribute
-    #returns the contact information based off input from desired attribute
-    # => does this by if statement on the instance variables?
+    end
   end
+
+   def search_by_attribute
+       puts "Select the contact field you wish to search by."
+       puts "[1] Search by first name"
+       puts "[2] Search by last name"
+       puts "[3] Search by email"
+       puts "[4] Search by note"
+       puts "[5] Exit"
+       puts "Enter a number:  "
+       contact = gets.chomp.to_i
+       case contact
+       when 1 then
+           puts "Enter contact's first name."
+           value = gets.chomp
+           return Contact.find_by(:first_name, value)
+         when 2 then
+           "Enter contact's last name."
+           last_name = gets.chomp
+           return Contact.find_by("last_name", last_name)
+         when 3 then
+           "Enter contact's email."
+           email = gets.chomp
+           return Contact.find_by("email", email)
+         when 4 then
+           "Enter the note for the contact."
+           note = gets.chomp
+           return Contact.find_by("note", note)
+         when 5 then exit
+     else
+       return "Oops, that didn't work! Try entering a different number."
+       contact = []
+     end
+   end
+ end
